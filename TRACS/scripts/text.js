15146f2ba5ab125a9c8d67805c8d767db597b5cc
David Creigh <David Creigh>
2016-02-17 01:25:38 +0000

    Added adjustable panels, live demo

---
+++ TRACS/scripts/text.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+		var fileInput = document.getElementById('fileInput');
+		var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+				console.log("Importing " + file.name);
+				process(fileDisplayArea, reader.result);
+			}
+
+
+			reader.readAsText(file);
+		});
+		
+}
+
+
+function process (fileDisplayArea, text) {
+	
+	while (fileDisplayArea.hasChildNodes()) {
+		fileDisplayArea.removeChild(fileDisplayArea.lastChild);
+	}
+	
+	
+	
+	lines = text.split("\n");
+	
+	var threadNode = document.createElement("div");
+	threadNode.setAttribute("class", "thread");
+	
+	for (i in lines) {
+        console.log(lines[i])
+		var newNode = document.createElement("div");
+		newNode.setAttribute("class", "statement");
+		newNode.innerText = lines[i];
+		newNode.setAttribute("id", "range_" + i)
+		threadNode.appendChild(newNode);
+	}
+	
+	fileDisplayArea.appendChild(threadNode)
+	document.getElementById("annotate").removeAttribute("disabled")
+	
+	localStorage.setItem("count", lines.length)
+}
